7f66b923d5dfcdaf0e711603d799e6ce395fdff0
Ryan Drake <ryan@existens.org>
2025-09-14 11:31:18 -0700

    If no output file is specified, just read and process the logs but do not copy

--- g3xlog.py
+++ g3xlog.py
@@ -22,14 +22,15 @@ def main():
         print("Error: Search path must be provided via G3X_SEARCH_PATH environment variable or command line argument", file=sys.stderr)
         sys.exit(1)
 
-    # Determine output path: command line > environment > current directory
-    log_path = args.output or os.getenv('G3X_LOG_PATH', os.getcwd())
+    # Search recursively for G3X log files (log_*.csv)
+    src_logs = sorted(glob.glob(mount_root + "/**/log_*.csv", recursive=True))
+
+    # Determine output path: command line > environment. If not specified, no files are output
+    log_path = args.output or os.getenv('G3X_LOG_PATH')
 
     # Create destination subfolders
-    [os.makedirs(log_path + f"/{subdir}", exist_ok=True) for subdir in ["config", "flight", "taxi"]]
-
-    # Search recursively for G3X log files (log_*.csv)
-    src_logs = glob.glob(mount_root + "/**/log_*.csv", recursive=True)
+    if log_path:
+        [os.makedirs(os.path.join(log_path, subdir), exist_ok=True) for subdir in ["config", "flight", "taxi"]]
 
     # Process each log source
     for log in src_logs:
@@ -53,38 +54,32 @@ def main():
                 key, value = match.groups()
                 metadata[key] = value
 
-        # Output metadata information if verbose mode is enabled
-        if args.verbose:
-            print(f"{os.path.basename(log)}: {metadata['aircraft_ident']} {metadata['product']} {metadata['unit']} {metadata['software_version']}")
-
         # Parse CSV
         df = pandas.read_csv(log, skiprows=[0,2])
 
         if df.empty:
             # If file has zero data, recommend deleting, for now just skip
-            if args.verbose:
-                print(f"{os.path.basename(log)}: empty")
+            flight_type = "empty"
         else:
             if df["Oil Press (PSI)"].max() < 1:
                 # If no oil pressure in all of log, assume this session was testing/configuration
-                dest_path = log_path + "/config/"
-                if args.verbose:
-                    print(f"{os.path.basename(log)}: config")
+                flight_type = "config"
             elif df["GPS Ground Speed (kt)"].max() < 50:
                 # If airplane did not achieve a ground speed sufficient for flight, assume taxi-only
-                dest_path = log_path + "/taxi/"
-                if args.verbose:
-                    print(f"{os.path.basename(log)}: taxi")
+                flight_type = "flight"
             else:
                 # Otherwise, the airplane was flying
-                dest_path = log_path + "/flight/"
-                if args.verbose:
-                    print(f"{os.path.basename(log)}: flight")
+                flight_type = "flight"
 
-            # Copy the file into the correct destination path, preserving modification time
-            dest_file = os.path.join(dest_path, os.path.basename(log))
-            if not os.path.exists(dest_file):
-                shutil.copy2(log, dest_file)
+            if log_path:
+                # Copy the file into the correct destination path, preserving modification time
+                dest_file = os.path.join(log_path, flight_type, os.path.basename(log))
+                if not os.path.exists(dest_file):
+                    shutil.copy2(log, dest_file)
+
+        # Print out flight type
+        if args.verbose:
+            print(f"{os.path.basename(log)}: {metadata['aircraft_ident']} {metadata['product']} {metadata['unit']} {metadata['software_version']} {flight_type}")
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
